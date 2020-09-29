8fcd23ca39b016389c0998a0f760369362a6dc2d
Edward <tardisman5197@users.noreply.github.com>
2020-09-29 08:41:51 +0000

    Create Simulation Struct

--- pkg/simulation/simulation.go
+++ pkg/simulation/simulation.go
@@ -4,8 +4,16 @@ import (
 	"fmt"
 )
 
+// Simulation holds all of the functionality
+// to start a barnes hut simulation.
+type Simulation struct {
+	grav   float64
+	theta  float64
+	Bodies []Body
+}
+
 // oneStep simulates on tick in the a simulation
-func oneStep(bodies []Body, grav, theta float64) []Body {
+func (s *Simulation) oneStep(bodies []Body) []Body {
 	// Create a new Oct Tree based on the bodies
 	root := NewRootNode(bodies)
 	root.BuildOcttree(bodies)
@@ -14,7 +22,7 @@ func oneStep(bodies []Body, grav, theta float64) []Body {
 	// Calculate and apply the forces to all
 	// the bodes in the simulation for a single
 	// tick.
-	root.CalcForces(grav, theta)
+	root.CalcForces(s.grav, s.theta)
 
 	// Display the resulting oct tree
 	fmt.Println(root.String(0))
@@ -23,10 +31,10 @@ func oneStep(bodies []Body, grav, theta float64) []Body {
 }
 
 // XSteps simulates a number of steps in a simulation
-func XSteps(steps int, bodies []Body, grav, theta float64) []Body {
+func (s *Simulation) XSteps(steps int) []Body {
 	for i := 0; i < steps; i++ {
 		fmt.Printf("---------- Step %v ----------\n", i)
-		bodies = oneStep(bodies, grav, theta)
+		s.Bodies = s.oneStep(s.Bodies)
 	}
-	return bodies
+	return s.Bodies
 }
