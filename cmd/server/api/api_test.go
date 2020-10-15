1dc6742769aa05a98e560535907abbf03e06d184
GaruGaru <garuglieritommaso.dev@gmail.com>
2020-10-15 11:41:46 +0200

    implemented results endpoint with tests

--- cmd/server/api/api_test.go
+++ cmd/server/api/api_test.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -56,7 +57,7 @@ func TestEndpointCreateSimulation(t *testing.T) {
 	}
 
 	currentSimulations := len(api.simulations)
-	if currentSimulations != 1{
+	if currentSimulations != 1 {
 		t.Fatalf("expected 1 simulation, found %d", currentSimulations)
 	}
 
@@ -98,7 +99,7 @@ func TestEndpointRemoveSimulation(t *testing.T) {
 	}
 
 	currentSimulations := len(api.simulations)
-	if currentSimulations != 1{
+	if currentSimulations != 1 {
 		t.Fatalf("expected 1 simulation, found %d", currentSimulations)
 	}
 
@@ -125,9 +126,34 @@ func TestEndpointRemoveSimulation(t *testing.T) {
 
 func TestRemoveNotPresentSimulation(t *testing.T) {
 	api := NewAPI()
+
+	reqBody := NewSimulationRequest{
+		Grav:   1,
+		Theta:  0,
+		Bodies: []simulation.Body{},
+	}
+
+	body, err := json.Marshal(reqBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request := &http.Request{
+		Method: http.MethodPost,
+		Body:   ioutil.NopCloser(bytes.NewBuffer(body)),
+	}
+
 	rr := httptest.NewRecorder()
 
-	request := mux.SetURLVars(&http.Request{
+	api.newSimulation(rr, request)
+
+	if rr.Result().StatusCode != http.StatusOK {
+		t.Fatalf("unexpected status code %d != %d", rr.Result().StatusCode, http.StatusOK)
+	}
+
+	rr = httptest.NewRecorder()
+
+	request = mux.SetURLVars(&http.Request{
 		Method: http.MethodGet,
 	}, map[string]string{
 		"simID": "test",
@@ -155,4 +181,86 @@ func TestRemoveWithoutSimID(t *testing.T) {
 	}
 }
 
+func TestSimulationStatusEndpoint(t *testing.T) {
+	api := NewAPI()
 
+	reqBody := NewSimulationRequest{
+		Grav:   1,
+		Theta:  0,
+		Bodies: []simulation.Body{},
+	}
+
+	body, err := json.Marshal(reqBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request := &http.Request{
+		Method: http.MethodPost,
+		Body:   ioutil.NopCloser(bytes.NewBuffer(body)),
+	}
+
+	rr := httptest.NewRecorder()
+
+	api.newSimulation(rr, request)
+
+	if rr.Result().StatusCode != http.StatusOK {
+		t.Fatalf("unexpected status code %d != %d", rr.Result().StatusCode, http.StatusOK)
+	}
+
+	var response NewSimulationResponse
+	if err := json.NewDecoder(rr.Result().Body).Decode(&response); err != nil {
+		t.Fatal(err)
+	}
+
+	if response.ID == "" {
+		t.Fatal("empty simulation ID")
+	}
+
+	rr = httptest.NewRecorder()
+
+	request = mux.SetURLVars(&http.Request{
+		Method: http.MethodGet,
+	}, map[string]string{
+		"simID": response.ID,
+	})
+
+	api.results(rr, request)
+
+	if rr.Result().StatusCode != http.StatusOK {
+		t.Fatalf("unexpected status code %d != %d", rr.Result().StatusCode, http.StatusOK)
+	}
+
+	var resultResponse simulationResultResponse
+	if err := json.NewDecoder(rr.Result().Body).Decode(&resultResponse); err != nil {
+		t.Fatal(err)
+	}
+
+	if resultResponse.Simulation.Grav != reqBody.Grav {
+		t.Fatalf("unexpected simulation Grav value %f != %f", resultResponse.Simulation.Grav, reqBody.Grav)
+	}
+
+	if resultResponse.Simulation.Theta != reqBody.Theta {
+		t.Fatalf("unexpected simulation Theta value %f != %f", resultResponse.Simulation.Theta, reqBody.Theta)
+	}
+
+	if reflect.DeepEqual(resultResponse.Simulation.Bodies, reqBody.Bodies) {
+		t.Fatalf("unexpected simulation Bodies values")
+	}
+
+}
+
+func TestSimulationStatusEndpointWithoutSimID(t *testing.T) {
+	api := NewAPI()
+	rr := httptest.NewRecorder()
+
+	request := mux.SetURLVars(&http.Request{
+		Method: http.MethodGet,
+	}, map[string]string{})
+
+	api.results(rr, request)
+
+	if rr.Result().StatusCode != http.StatusBadRequest {
+		t.Fatalf("unexpected status code %d != %d", rr.Result().StatusCode, http.StatusBadRequest)
+	}
+}
